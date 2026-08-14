cb8c321e898bf65dbd4bdf18018c698fc89cb996
Julian <theglinky@gmail.com>
2026-08-14 15:24:20 +0200

    Refactor build.gradle.kts for improved readability

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-plugins {
+        plugins {
     id("com.android.application")
     kotlin("android")
     kotlin("kapt")
@@ -47,32 +47,24 @@ android {
 }
 
 dependencies {
-    // Jetpack Compose - Core
     implementation("androidx.compose.ui:ui:1.6.1")
     implementation("androidx.compose.material3:material3:1.1.1")
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.1")
     debugImplementation("androidx.compose.ui:ui-tooling:1.6.1")
 
-    // Activity & Lifecycle
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
 
-    // Networking
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
-
-    // XML Parsing
     implementation("xmlpull:xmlpull:1.1.3.1")
 
-    // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
-    // Android Core
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
 
-    // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
