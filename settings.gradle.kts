1427f0c40656c73d26ad572ef369717951965d81
Julian <theglinky@gmail.com>
2026-08-14 16:06:41 +0200

    Remove plugin resolution strategy from settings
    Removed resolution strategy for plugin versions.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,15 +4,6 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-    resolutionStrategy {
-        eachPlugin {
-            when (requested.id.id) {
-                "com.android.application" -> useModule("com.android.tools.build:gradle:8.2.0")
-                "org.jetbrains.kotlin.android" -> useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-                "org.jetbrains.kotlin.kapt" -> useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-            }
-        }
-    }
 }
 
 dependencyResolutionManagement {
